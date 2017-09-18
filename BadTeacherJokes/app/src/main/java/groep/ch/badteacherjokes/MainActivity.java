fc89a37e8e3f57e0e0cdce6191e10ba42a639d2e
Marc Vollenweider <marc.vollenweider@edu.tbz.ch>
2017-09-18 14:04:03 +0200

    Project erstellt

---
+++ BadTeacherJokes/app/src/main/java/groep/ch/badteacherjokes/MainActivity.java
@@ -0,0 +1,13 @@
+package groep.ch.badteacherjokes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
